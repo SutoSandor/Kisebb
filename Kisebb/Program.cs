a9a3be7a361ba255c620bfe70eea8bb5e2781e01
SutoSandor <sandorsuto97@gmail.com>
2018-11-06 11:29:05 +0100

    Kisebb feladat

--- Kisebb/Program.cs
+++ Kisebb/Program.cs
@@ -8,8 +8,23 @@ namespace Kisebb
 {
     class Program
     {
+        private static double[] szamok = new double[] { 30.154, 35.924, 38.512, 38.129, 29.132, 24.002, 21.342, 18.201 };
+        private static double megszamol(double[] tomb)
+        {
+            double x = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] < 30)
+                {
+                    x = x + tomb[i];
+                }
+            }
+            return x;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("A 30-nál kisebb számok összege: " +megszamol(szamok));
+            Console.ReadKey();
         }
     }
 }
